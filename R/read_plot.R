ad940b56a2e1121e1603e056e7e91c6f2679b63d
andrewGhazi <6763470+andrewGhazi@users.noreply.github.com>
2026-07-26 12:45:26 -0400

    minor tweaks

--- R/read_plot.R
+++ R/read_plot.R
@@ -1,9 +1,11 @@
 strt = Sys.time()
 suppressPackageStartupMessages(library(data.table))
 
-get_t = \() format(Sys.time() - strt,
-                   digits = 3,
-                   units = 's')
+get_t = function() {
+  format(Sys.time() - strt,
+         digits = 3,
+         units = 's')
+}
 
 fread_class = function(f) {
   fread(f,
@@ -37,6 +39,8 @@ if (length(to_archive) > 0) {
     message("...in parallel.")
 
     library(mirai); daemons(4) # takes about 175ms
+
+    mirai::everywhere(library(data.table))
 
     dat <- mirai_map(fl[to_archive], fread_class)[]
 
@@ -123,30 +127,32 @@ message(paste0("Modify archive: ",
                       units = 's')))
 
 add_NA_by_seg = \(d) {
+  # Insert NAs onto each group by date segment --> break segments
+  # see ?lines
 
-  d[,rbindlist(list(.SD,
-                    data.table(t2 = NA_real_,
-                               value = NA_real_,
-                               last_24h = NA))), by = date_seg]
+  na_df = data.table(t2 = NA_real_,
+                     value = NA_real_,
+                     last_24h = NA)
+
+  d[,rbindlist(list(.SD, na_df)), by = date_seg]
 }
 
-pd = dat[variable == "PM10", .(t2, value, date_seg, last_24h,
-                               date_seg)] |>
+pd = dat[variable == "PM10",
+         .(t2, value, date_seg, last_24h, date_seg)] |>
   add_NA_by_seg()
-# pd[,rbind(.SD, data.table(t2 = NA, value = NA)), by = date_seg]
-# Insert NAs onto each group by date segment --> break segments
-# see ?lines
 
-l10 = pd[(last_24h)] |> add_NA_by_seg()
+l10 = pd[(last_24h)] |>
+  add_NA_by_seg()
 
-
-pd2 = dat[variable != "PM10", .(t2, value, date_seg, last_24h,
-                                date_seg)] |>
+pd2 = dat[variable != "PM10",
+          .(t2, value, date_seg, last_24h, date_seg)] |>
   add_NA_by_seg()
 
 l2 = pd2[(last_24h)] |>
   add_NA_by_seg()
 
+
+# top panel ---------------------------------------------------------------
 
 par(mfrow = c(2,1),
     mai = c(1.5, 3.2, 2, .5),
@@ -154,13 +160,13 @@ par(mfrow = c(2,1),
     bty = "n",
     family = "Arial")
 
-# top panel ---------------------------------------------------------------
+grid_h = seq(0,
+             10*floor(max(pd$value, na.rm = TRUE) / 10),
+             by = 10)
 
 plot(x = pd$t2, y = pd$value,
      col = rgb(0,0,0,.2),
-     panel.first = abline(h = seq(0,
-                                  10*floor(max(pd$value, na.rm = TRUE) / 10),
-                                  by = 10),
+     panel.first = abline(h = grid_h,
                           v = as.POSIXct("2025-11-21 00:00:00 EST") +
                                 seq(0,24, by = 6) * 3600,
                           lty = 'dotted',
@@ -182,15 +188,20 @@ points(dd$t2, dd$value,
        cex = .7,
        pch = 19)
 
-axis(2, lwd = 0, lwd.ticks = 1, cex.axis = .85,
+axis(2,
+     at = grid_h,
+     lwd = 0,
+     lwd.ticks = 1,
+     cex.axis = .75,
      tcl = .3,
-     padj = 1.2)
+     padj = 1.4)
 
 axis(1,
      at = as.POSIXct("2025-11-21 00:00:00 EST") +
             seq(0,24, by = 6) * 3600,
      labels = c("00:00", "06:00", "12:00", "18:00", "24:00"),
-     padj = -1.5,
+     padj = -2,
+     cex.axis = .75,
      lwd = 0)
 
 title(ylab = expression(paste(mu, "g / ", m^3, sep = "")),
@@ -202,11 +213,13 @@ title(main = "PM10",
 
 # bottom panel ------------------------------------------------------------
 
+grid_h2 = seq(0,
+              5*floor(max(pd2$value, na.rm = TRUE) / 5),
+              by = 5)
+
 plot(x = pd2$t2, y = pd2$value,
      col = rgb(0,0,0,.2),
-     panel.first = abline(h = seq(0,
-                                  5*floor(max(pd2$value, na.rm = TRUE) / 5),
-                                  by = 5),
+     panel.first = abline(h = grid_h2,
                           v = as.POSIXct("2025-11-21 00:00:00 EST") +
                                 seq(0,24, by = 6) * 3600,
                           lty = 'dotted',
@@ -230,17 +243,19 @@ points(dd$t2, dd$value,
 
 axis(2,
      lwd = 0,
+     at = grid_h2,
      lwd.ticks = 1,
-     cex.axis = .85,
+     cex.axis = .75,
      tcl = .3,
-     padj = 1.2)
+     padj = 1.4)
 
 axis(1,
      at = as.POSIXct("2025-11-21 00:00:00 EST") +
             seq(0,24, by = 6) * 3600,
      labels = c("00:00", "06:00", "12:00", "18:00", "24:00"),
      lwd = 0,
-     padj = -1.5)
+     cex.axis = .75,
+     padj = -2)
 
 title(ylab = expression(paste(mu, "g / ", m^3, sep = "")),
       line = 1.7)
